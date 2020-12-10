d8110d0f11e623c078830478d4ae4ee3372357bb
Anas <cawootte@live.com>
2020-12-10 20:48:32 +0100

    Sliding mode controller implementation V1.6

--- SMC.m
+++ SMC.m
@@ -1,10 +1,13 @@
-function [u] = SMC(e)
+function u  = SMC(S,Wd,Wr)
 %Sliding mode controller for a motor-propeller system
-%   Detailed explanation goes here
 %Model
 function y=f(x)
         y=-0.08653*x^2-39.902*x;
 end
-u=-f
+e=Wd-Wr;
+mu=0.02;
+beta=0.00015;
+sigma=10;
+u=-f(Wr)-sigma*e-(mu+beta)*sign(S);
 end
 
